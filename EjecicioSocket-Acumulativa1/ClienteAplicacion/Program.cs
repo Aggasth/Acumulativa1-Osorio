9cc3e2bbce34585146d5a0b23bc12c013098fbd7
Aggasth <101593156+Aggasth@users.noreply.github.com>
2022-07-13 15:52:20 -0400

    Add files via upload

---
+++ EjecicioSocket-Acumulativa1/ClienteAplicacion/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using ClienteLibrary;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClienteAplicacion
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese la direccíón del servidor:");
+            string servidor = Console.ReadLine().Trim();
+            int puerto;
+
+            do
+            {
+                Console.WriteLine("Ingrese el puerto a conectarse: ");
+            } while (!Int32.TryParse(Console.ReadLine().Trim(), out puerto));
+
+            Console.WriteLine("Conectado al servidor!");
+
+            Conect clienteSocket = new Conect(servidor,puerto);
+
+            if (clienteSocket.Conectar())
+            {
+                Comunicacion(clienteSocket);
+            }
+            else
+            {
+                Console.WriteLine("Se ha producido un error...");
+            }
+            Console.ReadKey();
+        }
+        static void Comunicacion(Conect clienteSocket)
+        {
+            Console.Clear();
+            bool terminar = false;
+            while (!terminar)
+            {
+                Console.WriteLine("Ingrese el mensaje:");
+                string mensaje = Console.ReadLine().Trim();
+                clienteSocket.Escribir(mensaje);
+
+                if (mensaje.ToLower() == "chao")
+                {
+                    terminar = true;
+                }
+                else
+                {
+                    mensaje = clienteSocket.Leer();
+                    if (mensaje != null)
+                    {
+                        Console.WriteLine("S: {0}", mensaje);
+                        if (mensaje.ToLower() == "chao")
+                        {
+                            terminar = true;
+                        }
+                    }
+                    else
+                    {
+                        terminar = true;
+                    }
+                }
+            }
+            clienteSocket.Desconectar();
+
+        }
+    }
+}
---
+++ EjecicioSocket-Acumulativa1/EjecicioSocket-Acumulativa1/Program.cs
@@ -0,0 +1,75 @@
+﻿using ServerLibrary;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjecicioSocket_Acumulativa1
+{
+    public class Program
+    {
+        private int puerto;
+        private Socket server;
+        static void Main(string[] args)
+        {
+            int puerto = Convert.ToInt32(ConfigurationManager.AppSettings["puerto"]);
+            Console.WriteLine("Iniciando servidor en puerto {0}", puerto);
+            ServerSocket serverSocket = new ServerSocket(puerto);
+            if (serverSocket.Iniciar())
+            {
+                while (true)
+                {
+                    Console.WriteLine("Esperando por un cliente...");
+                    Socket socket = serverSocket.ObtenerCliente();
+                    Console.WriteLine("Se ha conectado un cliente.");
+                    ClienteCom clienteCom = new ClienteCom(socket);
+                    Comunicacion(clienteCom);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Error con el puerto {0}, contactese con el admin", puerto);
+            }
+        }
+
+        static void Comunicacion(ClienteCom clienteCom)
+        {
+            bool terminar = false;
+            while (!terminar)
+            {
+                string mensaje = clienteCom.Leer();
+                if (mensaje != null)
+                {
+                    Console.WriteLine("Cliente: {0}", mensaje);
+                    if (mensaje.ToLower() == "chao")
+                    {
+                        terminar = true;
+                    }
+                    else
+                    {
+                        Console.Write("Ingrese Respuesta del Servidor: ");
+                        mensaje = Console.ReadLine().Trim();
+                        clienteCom.Escribir(mensaje);
+                        if (mensaje.ToLower() == "chao")
+                        {
+                            terminar = true;
+                        }
+
+                    }
+                }
+                else
+                {
+                    terminar = false;
+                }
+                if (terminar)
+                {
+                    clienteCom.Desconectar();
+                }
+            }
+            clienteCom.Desconectar();
+        }
+    }
+}
